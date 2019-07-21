ea5b501511d58fa370463b7a9350cd8845215ab1
Görge Albrecht <goerge@code-mentor.com>
2019-07-21 07:27:32 +0200

    TEST: first green

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -1,35 +1,46 @@
 package codingdojo;
 
+import java.util.Collections;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
-
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
 
-    // choose this one if you are familiar with mocks
-    @Ignore("Test is not finished yet")
-    @Test
-    public void damageCalculationsWithMocks() {
-        Inventory inventory = mock(Inventory.class);
-        Stats stats = mock(Stats.class);
-        SimpleEnemy target = mock(SimpleEnemy.class);
+  // choose this one if you are familiar with mocks
+  @Ignore("Test is not finished yet")
+  @Test
+  public void damageCalculationsWithMocks() {
+    Inventory inventory = mock(Inventory.class);
+    Stats stats = mock(Stats.class);
+    SimpleEnemy target = mock(SimpleEnemy.class);
 
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
-    }
+    Damage damage = new Player(inventory, stats).calculateDamage(target);
+    assertEquals(10, damage.getAmount());
+  }
 
-    // choose this one if you are not familiar with mocks
-    @Ignore("Test is not finished yet")
-    @Test
-    public void damageCalculations() {
-        Inventory inventory = new Inventory(null);
-        Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
-    }
+  // choose this one if you are not familiar with mocks
+  @Test
+  public void damageCalculations() {
+    Inventory inventory = new Inventory(defaultEquipment());
+    Stats stats = new Stats(10);
+    SimpleEnemy target = simpleEnemy();
+    Damage damage = new Player(inventory, stats).calculateDamage(target);
+    assertEquals(10, damage.getAmount());
+  }
+
+  private SimpleEnemy simpleEnemy() {
+    return new SimpleEnemy(new SimpleArmor(5), Collections.singletonList(new BasicBuff(1f, 1f)));
+  }
+
+  private Equipment defaultEquipment() {
+    return new Equipment(basicItem(), new BasicItem(10, 1), basicItem(), basicItem(), basicItem());
+  }
+
+  private BasicItem basicItem() {
+    return new BasicItem(0, 0);
+  }
 }
